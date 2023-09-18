147cf3f311f06c27f6b8f046ed865fdcf0014c99
Giiid <gideonfummey1@gmail.com>
2023-09-18 17:07:05 +0200

    Added get absolute url for id specification

--- second_web/models.py
+++ second_web/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
-class Services(models.Model):
-    name: models.CharField(max_length=100)
-    details: models.CharField(max_length=500)
+class NewServices(models.Model):
+    name = models.CharField(max_length=50)
+    details = models.CharField(max_length=1000)
+
+    def get_absolute_urls(self):
+        return reverse('details', kwargs={'id':self.id})
+
+class Blogs(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=10000)
+    content = models.CharField(max_length=1000000)   
+
+    def get_absolute_urls(self):
+        return reverse('single', kwargs={id:self.id}) 
 
