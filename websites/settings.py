5dc1785b94f9ad835f95aff7d7b6f1a042fbeab0
Giiid <120643201+giiiiid@users.noreply.github.com>
2024-05-31 16:48:50 +0100

    Update settings.py

--- websites/settings.py
+++ websites/settings.py
@@ -128,12 +128,7 @@ STATICFILES_DIRS = [BASE_DIR, 'static']
 MEDIA_URL = '' 
 
 # Sending emails
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'gideonfummey1@gmail.com'
-EMAIL_HOST_PASSWORD = '0544683777_momonumber'
+
 
 
 # Default primary key field type
